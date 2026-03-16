2ca6dbf2fd1adb58280071fa9349478337027f0f
Blue-chocolate <haasklany@gmail.com>
2026-03-16 21:32:19 +0200

    added validation rules

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,13 +3,42 @@
 namespace App\Http\Controllers;
 
 use Orion\Http\Controllers\Controller as ApiController;
+use Orion\Http\Requests\Request;
 
 class BlogController extends ApiController
 {
     protected $model = \App\Models\Blog::class;
-       // Allow public access - no auth required
+
     protected function authorizationRequired(): bool
     {
         return false;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'store' => [
+                'title'            => ['required', 'string', 'max:255'],
+                'slug'             => ['required', 'string', 'unique:blogs,slug'],
+                'short_description'=> ['required', 'string'],
+                'content'          => ['required', 'string'],
+                'author'           => ['required', 'string'],
+                'blog_category_id' => ['required', 'integer', 'exists:blog_categories,id'],
+                'image_path'       => ['nullable', 'string'],
+                'published_at'     => ['nullable', 'date'],
+                'is_published'     => ['nullable', 'boolean'],
+            ],
+            'update' => [
+                'title'            => ['sometimes', 'string', 'max:255'],
+                'slug'             => ['sometimes', 'string', 'unique:blogs,slug'],
+                'short_description'=> ['sometimes', 'string'],
+                'content'          => ['sometimes', 'string'],
+                'author'           => ['sometimes', 'string'],
+                'blog_category_id' => ['sometimes', 'integer', 'exists:blog_categories,id'],
+                'image_path'       => ['nullable', 'string'],
+                'published_at'     => ['nullable', 'date'],
+                'is_published'     => ['nullable', 'boolean'],
+            ],
+        ];
     }
 }
